a3ed3acdffe43c83613e9db0a6ac5dab0b3e876a
kumaraish <kumar.aish2010@gmail.com>
2016-03-12 00:31:11 +0530

    Initial commit uva 100

---
+++ 100.cpp
@@ -0,0 +1,61 @@
+/**
+ * AC - 0.037 secs
+ * Further optimizations after simple cache -
+ * Replaced division / multiplication with bit operators
+ * No improvements in speed :-(
+ */
+
+#include <iostream>
+#include <stdio.h>
+
+int Table[1000000] = {0};
+
+int getCycleLen(int k) {
+    int len = 1;
+
+    while (k > 1) {
+        if (k & 0x0001) {
+            k = (k + k + k) + 1;
+        } else {
+            k = k >> 1;
+        }
+        len++;
+    }
+
+    return len;
+}
+
+int main() {
+
+#ifndef ONLINE_JUDGE
+
+#include "TestcasesFilePath.h"
+
+    freopen(Testcases_File_Path
+                    "100.in", "r", stdin);
+#endif
+
+    int i, j, maxCycleLen;
+
+    while (std::cin >> i >> j) {
+        int orig_i = i;
+        int orig_j = j;
+        if (i > j) {
+            std::swap(i, j);
+        }
+
+        maxCycleLen = 0;
+        for (int k = i; k <= j; k++) {
+
+            if (Table[k] == 0) {
+                Table[k] = getCycleLen(k);
+            }
+
+            maxCycleLen = std::max(Table[k], maxCycleLen);
+        }
+
+        std::cout << orig_i << " " << orig_j << " " << maxCycleLen << std::endl;
+    }
+
+    return 0;
+}
